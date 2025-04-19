9f00fa4e27c74ab9b6ca7db4de30288312aca819
yzemlyak <yzemlyak@gmail.com>
2025-04-19 23:09:05 +0000

    First route done

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/square/new") do
+  erb(:new_square_calc)
+end 
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
