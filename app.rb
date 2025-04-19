00ee3053839c0f5087a604465332a62df5a5fa91
yzemlyak <yzemlyak@gmail.com>
2025-04-19 23:42:40 +0000

    Minor adjustment - first 2

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get("/square/new") do
 end 
 
 get("/square/results") do
-  @the_num = params.fetch("users_number").to_f
+  @the_num = params.fetch("user_number").to_f
   @the_result = @the_num ** 2
   erb(:square_results)
 end
@@ -16,7 +16,7 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
-  @the_num = params.fetch("users_number").to_f
+  @the_num = params.fetch("user_number").to_f
   @the_result = @the_num ** 0.5
   erb(:square_root_results)
 end
