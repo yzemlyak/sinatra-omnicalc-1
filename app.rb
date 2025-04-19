f0657da1b18a9b5c38681268c6616c29d0b96e3c
yzemlyak <yzemlyak@gmail.com>
2025-04-19 23:40:22 +0000

    Added sqrt

--- app.rb
+++ app.rb
@@ -11,6 +11,16 @@ get("/square/results") do
   erb(:square_results)
 end
 
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 0.5
+  erb(:square_root_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
